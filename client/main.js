69118630f48beebe75dd3e2cf5cdad9a901b7d09
LxSwiss <lxswiss@gmail.com>
2017-02-15 12:06:14 +0100

    adds function which iterates through all arrondisements

--- client/main.js
+++ client/main.js
@@ -4,29 +4,30 @@ Markers = new Mongo.Collection('markers');
 let paris = {lat: 48.864716, lng: 2.349014};
 let diderot = {lat: 48.492819, lng: 2.223059};
 let kremlin = {lat: 48.81471, lng: 2.36073};
-let touchplace = {lat: 48.87397066741686, lng: 2.396054267701402};
 
-// LatLng from Arrondisements in Paris
-let arr1= {lat: 48.8592, lng: 2.3417};
-let arr2= {lat: 48.8655, lng: 2.3426};
-let arr3= {lat: 48.8637, lng: 2.3615};
-let arr4= {lat: 48.8601, lng: 2.3507};
-let arr5= {lat: 48.8448, lng: 2.3471};
-let arr6= {lat: 48.8493, lng: 2.33};
-let arr7= {lat: 48.8565, lng: 2.321};
-let arr8= {lat: 48.8763, lng: 2.3183};
-let arr9= {lat: 48.8718, lng: 2.3399};
-let arr10= {lat: 48.8709, lng: 2.3561};
-let arr11= {lat: 48.8574, lng: 2.3795};
-let arr12= {lat: 48.8412, lng: 2.3876};
-let arr13= {lat: 48.8322, lng: 2.3561};
-let arr14= {lat: 48.8331, lng: 2.3264};
-let arr15= {lat: 48.8412, lng: 2.3003};
-let arr16= {lat: 48.8637, lng: 2.2769};
-let arr17= {lat: 48.8835, lng: 2.3219};
-let arr18= {lat: 48.8925, lng: 2.3444};
-let arr19= {lat: 48.8817, lng: 2.3822};
-let arr20= {lat: 48.8646, lng: 2.3984};
+// LatLng from Arrondissements in Paris
+let arr1  = {lat: 48.8592, lng: 2.3417};
+let arr2  = {lat: 48.8655, lng: 2.3426};
+let arr3  = {lat: 48.8637, lng: 2.3615};
+let arr4  = {lat: 48.8601, lng: 2.3507};
+let arr5  = {lat: 48.8448, lng: 2.3471};
+let arr6  = {lat: 48.8493, lng: 2.33};
+let arr7  = {lat: 48.8565, lng: 2.321};
+let arr8  = {lat: 48.8763, lng: 2.3183};
+let arr9  = {lat: 48.8718, lng: 2.3399};
+let arr10  = {lat: 48.8709, lng: 2.3561};
+let arr11  = {lat: 48.8574, lng: 2.3795};
+let arr12  = {lat: 48.8412, lng: 2.3876};
+let arr13  = {lat: 48.8322, lng: 2.3561};
+let arr14  = {lat: 48.8331, lng: 2.3264};
+let arr15  = {lat: 48.8412, lng: 2.3003};
+let arr16  = {lat: 48.8637, lng: 2.2769};
+let arr17  = {lat: 48.8835, lng: 2.3219};
+let arr18  = {lat: 48.8925, lng: 2.3444};
+let arr19  = {lat: 48.8817, lng: 2.3822};
+let arr20  = {lat: 48.8646, lng: 2.3984};
+
+let arrondissements = [arr1,arr2,arr3,arr4,arr5,arr6,arr7,arr8,arr9,arr10,arr11,arr12,arr13,arr14,arr15,arr16,arr17,arr18,arr19,arr20, kremlin];
 
 
 
@@ -40,6 +41,7 @@ let infowindow;
 Template.map.onCreated(function() {
   GoogleMaps.ready('map', function(map) {
     infowindow = new google.maps.InfoWindow();
+    var service = new google.maps.places.PlacesService(map.instance);
 
     google.maps.event.addListener(map.instance, 'click', function(event) {
         //Meteor.call("checkPlacesNearby", function(error, results) {
@@ -50,11 +52,50 @@ Template.map.onCreated(function() {
       console.log(touchplace);
     });
 
+    let request;
+    console.log(arrondissements[0])
+    if(Markers.find().count() < 50  ){
+
+      for (var i = 0; i < arrondissements.length; i++){ 
+
+        let arrondissement = arrondissements[i];
+      // Specify location, radius and place types for your Places API search.
+        request = {
+          location: arrondissement,
+          radius: '1000',
+          types: ['library','cafe','university'],
+          rankby: google.maps.places.RankBy.PROMINENCE
+        };
+
+          // Create the PlaceService and send the request.
+  // Handle the callback with an anonymous function.
+        service.nearbySearch(request, function(results, status) {
+          if (status == google.maps.places.PlacesServiceStatus.OK) {
+            for (var i = 0; i < results.length; i++) {
+              var place = results[i];
+
+            // If the request succeeds, insert the marker into the database
+            // upsert only inserts if place.id doesnt exist yet
+
+            Markers.upsert(place.place_id, {  
+              //Modifier
+              $set: {
+                lat: place.geometry.location.lat(),
+                lng: place.geometry.location.lng(),
+                name: place.name,
+                place_id: place.place_id
+              }
+            });
+          }
+        }
+      });
+
+      }    
+    }
     
 
-
       // Specify location, radius and place types for your Places API search.
-    let request = {
+    request = {
         location: paris,
         radius: '1000',
         types: ['library','cafe','university'],
@@ -64,8 +105,6 @@ Template.map.onCreated(function() {
 
   // Create the PlaceService and send the request.
   // Handle the callback with an anonymous function.
-    
-    var service = new google.maps.places.PlacesService(map.instance);
     service.nearbySearch(request, function(results, status) {
         if (status == google.maps.places.PlacesServiceStatus.OK) {
           for (var i = 0; i < results.length; i++) {
@@ -74,11 +113,7 @@ Template.map.onCreated(function() {
             // If the request succeeds, insert the marker into the database
             // upsert only inserts if place.id doesnt exist yet
 
-            Markers.upsert(
-            
-              //selector
-              place.place_id
-            , {  
+            Markers.upsert(place.place_id, {  
               //Modifier
               $set: {
                 lat: place.geometry.location.lat(),
