b0c345c6d07ae6051181ea063389b141daaab5da
LxSwiss <lxswiss@gmail.com>
2017-03-10 13:24:50 +0100

    adds js for infowindow styling

--- client/main.js
+++ client/main.js
@@ -199,6 +199,36 @@ Template.map.onCreated(function() {
         //  Markers.update(marker.id, { $set: { lat: event.latLng.lat(), lng: event.latLng.lng() }});
         //});
         let place_info;
+
+
+        google.maps.event.addListener(infowindow, 'domready', function() {
+
+   // Reference to the DIV which receives the contents of the infowindow using jQuery
+   var iwOuter = $('.gm-style-iw');
+
+   /* The DIV we want to change is above the .gm-style-iw DIV.
+    * So, we use jQuery and create a iwBackground variable,
+    * and took advantage of the existing reference to .gm-style-iw for the previous DIV with .prev().
+    */
+   var iwBackground = iwOuter.prev();
+
+   // Remove the background shadow DIV
+   iwBackground.children(':nth-child(2)').css({'display' : 'none'});
+
+   // Remove the white background DIV
+   iwBackground.children(':nth-child(4)').css({'display' : 'none'});
+
+});
+
+
+
+
+
+
+
+
+
+
         google.maps.event.addListener(marker, 'click', function(){
           infowindow.setContent("");
           service.getDetails({
