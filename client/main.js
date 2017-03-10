e2e316179d78100050432c0e72292898f9f32817
LxSwiss <lxswiss@gmail.com>
2017-03-10 13:49:01 +0100

    some more styling of infowindow

--- client/main.js
+++ client/main.js
@@ -200,35 +200,6 @@ Template.map.onCreated(function() {
         //});
         let place_info;
 
-
-        google.maps.event.addListener(infowindow, 'domready', function() {
-
-   // Reference to the DIV which receives the contents of the infowindow using jQuery
-   var iwOuter = $('.gm-style-iw');
-
-   /* The DIV we want to change is above the .gm-style-iw DIV.
-    * So, we use jQuery and create a iwBackground variable,
-    * and took advantage of the existing reference to .gm-style-iw for the previous DIV with .prev().
-    */
-   var iwBackground = iwOuter.prev();
-
-   // Remove the background shadow DIV
-   iwBackground.children(':nth-child(2)').css({'display' : 'none'});
-
-   // Remove the white background DIV
-   iwBackground.children(':nth-child(4)').css({'display' : 'none'});
-
-});
-
-
-
-
-
-
-
-
-
-
         google.maps.event.addListener(marker, 'click', function(){
           infowindow.setContent("");
           service.getDetails({
@@ -255,6 +226,35 @@ Template.map.onCreated(function() {
           infowindow.open(map, this);
 
           
+        });
+
+
+        /*
+         * The google.maps.event.addListener() event waits for
+         * the creation of the infowindow HTML structure 'domready'
+         * and before the opening of the infowindow defined styles
+         * are applied.
+         */
+        google.maps.event.addListener(infowindow, 'domready', function() {
+
+           // Reference to the DIV which receives the contents of the infowindow using jQuery
+           var iwOuter = $('.gm-style-iw');
+
+           /* The DIV we want to change is above the .gm-style-iw DIV.
+            * So, we use jQuery and create a iwBackground variable,
+            * and took advantage of the existing reference to .gm-style-iw for the previous DIV with .prev().
+            */
+           var iwBackground = iwOuter.prev();
+
+           // Remove the background shadow DIV
+           iwBackground.children(':nth-child(2)').css({'display' : 'none'});
+
+           // Remove the white background DIV
+           iwBackground.children(':nth-child(4)').css({'display' : 'none'});
+
+           var iwCloseBtn = iwOuter.next();
+           iwCloseBtn.css({'display': 'none'});
+
         });
 
         markers[document._id] = marker;
